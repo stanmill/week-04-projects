0e6bd381a86c5d5d4413b0bbfc1b9aaab1af3f2f
stanmill <stanley.pena29@gmail.com>
2018-10-06 12:40:55 -0400

    update

--- zip-api/app.js
+++ zip-api/app.js
@@ -16,11 +16,15 @@ app.get('/', (req, res) => {
 
 app.get('/zip/:zipcode', (req, res) => {
   // fill in...
+  const zipcode = zipdb.byZip.zipcode;
+  req.json(zipcode);
+  res.send(`the zipcode is ${zipcode}`);
 });
 
 
 app.get('/city/:cityname', (req, res) => {
   // fill in...
+  const cityname = zipdb.byCity.cityname;
 });
 
 
